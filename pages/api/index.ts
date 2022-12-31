064d435bb603816c552bfe690ad835eab713dc95
Elozino <ovedhee@gmail.com>
2022-12-31 18:36:58 +0100

    type error

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,12 +1,11 @@
-import {  AxiosResponse } from "axios";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { AxiosResponse } from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   organization: "org-1HZ2rofgie6e7gyc2ZTt6O66",
-  // apiKey: process.env.OPENAI_API_KEY,
-  apiKey: "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",
- });
+  apiKey: "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",
+});
 
 const openai = new OpenAIApi(configuration);
 
@@ -25,16 +24,15 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   const { message } = req.body;
-
+  console.log({ message });
   if (req.method === "POST") {
     try {
-      const response: AxiosResponse<CreateCompletionResponse> =
-        await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt: message,
-          max_tokens: 100,
-          temperature: 0.5,
-        });
+      const response: AxiosResponse<CreateCompletionResponse> = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: message,
+        max_tokens: 100,
+        temperature: 0.5,
+      });
       res.status(200).json(response.data.choices[0].text);
     } catch (error) {
       console.log(error);
