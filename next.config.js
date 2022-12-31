329425629dca0809a9d93c7a23242e5b65aece1e
Elozino <ovedhee@gmail.com>
2022-12-31 22:22:35 +0100

    config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  env: { NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY },
 };
 
 module.exports = nextConfig;
