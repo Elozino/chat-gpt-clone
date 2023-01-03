0afa95f8d1f95efc63c7f67455a3498be89da2b5
Elozino <ovedhee@gmail.com>
2023-01-03 01:22:24 +0100

    fixes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 export const Sidebar = ({ clearChat }: IProps) => {
   return (
-    <div className='w-1/5 md:w-80 bg-appDarkBg p-2'>
+    <div className='hidden md:block :w-1/5 md:w-80 bg-appDarkBg p-2'>
       <div
         onClick={clearChat}
         className='rounded-md border border-white p-2 px-4 text-white hover:text-appDarkBg hover:bg-gray-50 cursor-pointer'>
